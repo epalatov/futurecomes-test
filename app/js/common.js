957c29be79c09fc36073f62072793253e31d4dc7
Gentim Dao <befrontender@gmail.com>
2017-03-31 10:17:43 +0700

    Adds the slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
 $(function() {
-
-	// Custom JS
-
+	$(".owl-carousel").owlCarousel({
+		navigation : true,
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		pagination: false,
+		rewindSpeed: 500,
+		items: 1
+	});
 });
